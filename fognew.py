88359862220a110c42f05503a1c6179b11faa262
marcusbdc <90267595+marcusbdc@users.noreply.github.com>
2023-04-20 22:35:59 -0400

    Update fognew.py

--- fognew.py
+++ fognew.py
@@ -1,68 +1,63 @@
 import os
-import numpy as np 
+import numpy as np
 import pandas as pd
-import os
-from tqdm import tqdm
 from glob import glob
+from tqdm import tqdm
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn import ensemble, linear_model
 from sklearn.metrics import average_precision_score
 
-ROOT_DIR = "/kaggle/input/tlvmc-parkinsons-freezing-gait-prediction"
-FEATURES_LIST = ["AccV", "AccML", "AccAP"]
-LABELS_LIST = ["StartHesitation", "Turn", "Walking"]
+import xgboost as xgb
+
+root_dir = "/kaggle/input/tlvmc-parkinsons-freezing-gait-prediction"
+features = ["AccV", "AccML", "AccAP"]
+labels = ["StartHesitation", "Turn", "Walking"]
 
 def read_data(dataset, datatype):
-    metadata_df = pd.read_csv(os.path.join(ROOT_DIR,dataset + "_metadata.csv"))
+    metadata = pd.read_csv(os.path.join(root_dir, f"{dataset}_metadata.csv"))
     
-    file_path = os.path.join(ROOT_DIR, datatype, dataset)
+    file_path = os.path.join(root_dir, datatype, dataset)
     
-    df_res = pd.DataFrame()
-    for _, _, files in os.walk(file_path):
-        for name in files:
-            f_path = os.path.join(file_path, name)
-            csv_df = pd.read_csv(f_path)
-            csv_df["file"] = name.replace(".csv", "")
-            df_res = pd.concat([df_res,csv_df])
+    df_res = pd.concat([
+        pd.read_csv(os.path.join(file_path, name)).assign(file=name.replace(".csv", ""))
+        for _, _, files in os.walk(file_path)
+        for name in files
+    ])
     
-    df_res = metadata_df.merge(df_res, how = 'inner', left_on = 'Id', right_on = 'file')
-    df_res = df_res.drop(["file"], axis = 1)
+    df_res = metadata.merge(df_res, how="inner", left_on="Id", right_on="file").drop("file", axis=1)
         
     return df_res
     
-df_train_defog = read_data('defog','train')
-df_train_tdcsfog = read_data('tdcsfog', 'train')
+df_train_defog = read_data("defog", "train")
+df_train_tdcsfog = read_data("tdcsfog", "train")
 df_train = pd.concat([df_train_defog, df_train_tdcsfog])
-X_train, X_valid, y_train, y_valid = train_test_split(df_train[FEATURES_LIST], df_train[LABELS_LIST], test_size=0.30, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(df_train[features], df_train[labels], test_size=0.3, random_state=42)
 
-import xgboost as xgb
 dtrain_reg = xgb.DMatrix(X_train, y_train, enable_categorical=True)
 dtest_reg = xgb.DMatrix(X_valid, y_valid, enable_categorical=True)
 
 params = {"objective": "reg:squarederror", "tree_method": "gpu_hist"}
-n_trees = 100
-model = xgb.train(
-   params=params,
-   dtrain=dtrain_reg,
-   num_boost_round=n_trees,
-)
-print(average_precision_score(y_valid, model.predict(dtest_reg).clip(0.0,1.0)))
+n_rounds = 100
+model = xgb.train(params=params, dtrain=dtrain_reg, num_boost_round=n_rounds)
 
-sample_submission_df = pd.read_csv(os.path.join(ROOT_DIR,'sample_submission.csv'))
-test_files_list = glob(os.path.join(ROOT_DIR,'test/**/**'))
+y_pred = model.predict(dtest_reg).clip(0.0, 1.0)
+print(average_precision_score(y_valid, y_pred))
 
-sample_submission_df['sample'] = 0
-submission_df_list = []
-for file_path in test_files_list:
-    df = pd.read_csv(file_path)
-    df['Id'] = file_path.split('/')[-1].split('.')[0]
+sample_submission = pd.read_csv(os.path.join(root_dir, "sample_submission.csv"))
+test_files = glob(os.path.join(root_dir, "test/**/**"))
+
+df_list = []
+for file in tqdm(test_files, desc="Processing test files"):
+    df = pd.read_csv(file)
+    df["Id"] = os.path.splitext(os.path.basename(file))[0]
     df = df.fillna(0).reset_index(drop=True)
-    res_df = pd.DataFrame(np.round(model.predict(xgb.DMatrix(df[FEATURES_LIST])),3), columns=LABELS_LIST)
-    df = pd.concat([df,res_df], axis=1)
-    df['Id'] = df['Id'].astype(str) + '_' + df['Time'].astype(str)
-    submission_df_list.append(df[['Id','StartHesitation', 'Turn' , 'Walking']])
-submission_df = pd.concat(submission_df_list)
-submission_df = pd.merge(sample_submission_df[['Id','sample']], submission_df, how='left', on='Id').fillna(0.0)
-submission_df[['Id','StartHesitation', 'Turn' , 'Walking']].to_csv('submission.csv', index=False)
+    res = pd.DataFrame(np.round(model.predict(xgb.DMatrix(df[features])), 3), columns=labels)
+    df = pd.concat([df, res], axis=1)
+    df["Id"] = df["Id"].astype(str) + "_" + df["Time"].astype(str)
+    df_list.append(df[["Id", "StartHesitation", "Turn", "Walking"]])
+
+submission_df = pd.concat(df_list)
+submission_df = pd.merge(sample_submission[["Id", "sample"]], submission_df, how="left", on="Id").fillna(0.0)
+submission_df[["Id", "StartHesitation", "Turn", "Walking"]].to_csv("submission.csv", index=False)
